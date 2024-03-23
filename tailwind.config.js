aeddd8c77931f9492c9c887747b471cffc6154e5
Technovation-2K24 <133515128+Technovation-2K24@users.noreply.github.com>
2024-03-23 19:06:23 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
   content: ["./public/*.html"],
   theme: {
     extend: {
-      'hero-image':"url('./public/assets/hero_section.jpg')"
+      'hero-image':"url('./public/assets/hero_section.jpg')",
+      fontSize: {
+        xs: "0.75rem",
+        sm: "0.875rem",
+        base: "1rem",
+        lg: "1.125rem",
+        xl: "1.25rem",
+        "2xl": "1.5rem",
+        "3xl": "1.875rem",
+        "4xl": "2.25rem",
+        "5xl": "3rem",
+        "6xl": "4rem",
+      },
     },
   },
   plugins: [],
